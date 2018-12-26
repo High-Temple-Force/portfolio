3833cb07c20ebdd5f5d97eb396feb862bf49e547
EC2 Default User <ec2-user@ip-10-0-1-9.ap-northeast-1.compute.internal>
2018-12-26 05:25:37 +0000

    make blog

---
+++ script.js
@@ -0,0 +1,21 @@
+var target = document.getElementsByClassName('article');
+
+for (let i = 0; i < target.length; i++) {
+    //ON
+    setborder(target[i],
+        'mouseenter',
+        "2px solid rgba(241, 159, 64,1)");
+    //OUT
+    setborder(target[i],
+        'mouseleave',
+        "2px solid rgba(241, 159, 64,0.5)");
+}
+function getelement(element, mouse) {
+    element.addEventListener()
+}
+function setborder(element, mouse, border) {
+    element.addEventListener(mouse, () => {
+        element.style.border = border;
+        
+    }, false);
+}
